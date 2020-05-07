28ac54eb62b10b88e1949bf127e320a98ad51ba5
linhall <lehwx87@gmail.com>
2020-05-06 21:49:29 -0400

    testing

--- musician/settings.py
+++ musician/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
